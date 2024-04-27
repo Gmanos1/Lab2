c61cdaba4444db720601d6e2d282e304b692743b
MS_ios <user@account.com>
2024-04-27 17:09:46 +0200

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
